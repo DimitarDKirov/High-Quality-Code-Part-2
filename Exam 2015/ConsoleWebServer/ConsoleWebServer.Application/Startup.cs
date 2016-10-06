269f7374b0143f14705ba75ac063506d20664d6c
DimitarDKirov <dimkirov@hotmail.com>
2016-10-06 19:38:06 +0300

    Added HQC exam 2015 to practice

---
+++ Exam 2015/ConsoleWebServer/ConsoleWebServer.Application/Startup.cs
@@ -0,0 +1,9 @@
+﻿// TODO: Describe patterns, SOLID, bugs and bottleneck in Documentation.txt
+public static class БръмБръм
+{
+    public static void Main()
+    {
+        var webSever = new WebServerConsole();
+        webSever.Start();
+    }
+};
